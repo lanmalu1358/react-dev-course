2c336e8fed31d52ae293d0b9282c05e0bc058c08
lanmalu1358 <chousyosinsya@g-mail.com>
2020-08-08 18:57:25 +0900

    コミット３

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return (//returnの中がjsx
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          hello world
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Hello react
         </a>
       </header>
     </div>
