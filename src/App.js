2f60e1522dce979ca788f0353ddfdfa1929751b5
lanmalu1358 <chousyosinsya@g-mail.com>
2020-08-22 18:55:56 +0900

    コミット4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+{
+  document.title = "Okada"
+}
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    console.log(count);
+  }
+
+  const name = 'okada';
+
+  const handleclick = () => {
+    window.alert("Hello World")
+  }
   return (//returnの中がjsx
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-        >
-          Hello react
+
+        <button onClick={increment}>いいね！</button>
+        {count}
+
+        <a className="App-link">
+          <button
+            onClick={handleclick}
+          >Hello World</button>
         </a>
+        <div>
+          <h1>Hello World</h1>
+          <h2>Hello World</h2>
+          <h3>Hello World</h3>
+          <h4>Hello World</h4>
+
+          <button onClick={() => window.alert("Hello World")}>button</button>
+
+          <button>button</button>
+          <input type="text" />
+          <a href="#">a タグ</a>
+
+          {name}
+
+        </div>
       </header>
     </div>
   );
