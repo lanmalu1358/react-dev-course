ec937b69ce985e84dd26c3e94e0be8584d9757aa
lanmalu1358 <chousyosinsya@g-mail.com>
2020-11-21 18:57:44 +0900

    コミット11

--- src/ComponentB.js
+++ src/ComponentB.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Table } from 'react-bootstrap'
 import axios from 'axios'
@@ -18,11 +18,44 @@ const ComponentB = (props) => {
             )
     }, []);
 
+
     return (
+
         <>
             <div>ComponentB</div>
             <button onClick={backpage}>ホームへ戻る</button>
             <Link to="componentc">ComponentCへ移動</Link>
+
+
+
+            {/* res.data.userID         userID,ID,Title,Body*/}
+            {
+
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>userID</th>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Body</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </Table>
+
+            }
+
+            <button href='https://jsonplaceholder.typicode.com/posts'>
+                <a href='https://jsonplaceholder.typicode.com/posts'>GetData</a>
+            </button>
+
 
             <Table striped bordered hover variant="dark">
                 <thead>
